155bca7772d79b2046e5a3a8a9113a5b4ada3982
aaryashirke2005 <aaryashirke2005@gmail.com>
2026-03-14 22:42:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,11 +101,6 @@ def sitemap():
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/robots.txt")
-def robots():
-    return send_from_directory("static", "robots.txt", mimetype="text/plain")
-
 @app.route("/sitemap.xml")
 def sitemap():
     return send_from_directory("static", "sitemap.xml", mimetype="application/xml")
